f44c7c808534788856fab78402f7d95f0db592ba
Rein-xmmm <72437331+Rein-xmmm@users.noreply.github.com>
2020-10-16 16:34:19 +0900

    Initial commit

---
+++ mm/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            { 
+                Console.WriteLine("Введите число ->");
+                int chislo = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine($"Ваше число-{chislo}");
+            }
+           
+            
+            catch (Exception)
+            { 
+                Console.WriteLine("Эмм -,-) у вас с математикой проблемы? ");
+            }
+
+             Console.ReadLine();
+        
+        }
+    }
+}
